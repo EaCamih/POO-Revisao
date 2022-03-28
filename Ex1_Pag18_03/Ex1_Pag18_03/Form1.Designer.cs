a62958eec573db4a4b15a8aa9c3a0fe4ad3ff10b
MarcL <70085700+MarcLDev@users.noreply.github.com>
2022-03-28 13:36:16 -0300

    Exercício 03 Página 18

---
+++ Ex1_Pag18_03/Ex1_Pag18_03/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Ex1_Pag18_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(251, 98);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(251, 72);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(115, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Número do Mês";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 222);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Ex1_Pag18_03/Ex1_Pag18_03/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex1_Pag18_03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNumero.Text))
+            {
+                MessageBox.Show("TextBox Vazio.");
+            }
+            else
+            {
+                int numero = Convert.ToInt32(txtNumero.Text);
+                if(numero < 1 || numero > 12)
+                {
+                    MessageBox.Show("Número deve ser entre 1 e 12");
+                }
+                else
+                {
+                    switch (numero)
+                    {
+                        case 1:
+                            MessageBox.Show("O mês de janeiro tem 31 dias.");
+                            break;
+
+
+                        case 2:
+                            MessageBox.Show("O mês de fevereiro tem 28 ou 29 dias.");
+                            break;
+
+                        case 3:
+                            MessageBox.Show("O mês de março tem 31 dias.");
+                            break;
+
+                        case 4:
+                            MessageBox.Show("O mês de abril tem 30 dias.");
+                            break;
+
+                        case 5:
+                            MessageBox.Show("O mês de maio tem 31 dias.");
+                            break;
+
+                        case 6:
+                            MessageBox.Show("O mês de junho tem 30 dias.");
+                            break;
+
+                        case 7:
+                            MessageBox.Show("O mês de julho tem 31 dias.");
+                            break;
+
+                        case 8:
+                            MessageBox.Show("O mês de agosto tem 31 dias.");
+                            break;
+
+                        case 9:
+                            MessageBox.Show("O mês de setembro tem 30 dias.");
+                            break;
+
+                        case 10:
+                            MessageBox.Show("O mês de outubro tem 31 dias.");
+                            break;
+
+                        case 11:
+                            MessageBox.Show("O mês de novembro tem 30 dias.");
+                            break;
+
+                        case 12:
+                            MessageBox.Show("O mês de dezembro tem 31 dias.");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
