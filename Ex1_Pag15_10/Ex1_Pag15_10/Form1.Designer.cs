958a508497ea5d5b8d3569c0fc91f897f96fd932
MarcL <70085700+MarcLDev@users.noreply.github.com>
2022-03-29 13:52:50 -0300

    Finalmente o último Yaaay

---
+++ Ex1_Pag15_10/Ex1_Pag15_10/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace Ex1_Pag15_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.cb2 = new System.Windows.Forms.ComboBox();
+            this.cb3 = new System.Windows.Forms.ComboBox();
+            this.cb4 = new System.Windows.Forms.ComboBox();
+            this.cb5 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cb1
+            // 
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Items.AddRange(new object[] {
+            "Sim",
+            "Não"});
+            this.cb1.Location = new System.Drawing.Point(434, 45);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(121, 21);
+            this.cb1.TabIndex = 0;
+            // 
+            // cb2
+            // 
+            this.cb2.FormattingEnabled = true;
+            this.cb2.Items.AddRange(new object[] {
+            "Sim",
+            "Não"});
+            this.cb2.Location = new System.Drawing.Point(434, 72);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(121, 21);
+            this.cb2.TabIndex = 1;
+            // 
+            // cb3
+            // 
+            this.cb3.FormattingEnabled = true;
+            this.cb3.Items.AddRange(new object[] {
+            "Sim",
+            "Não"});
+            this.cb3.Location = new System.Drawing.Point(434, 99);
+            this.cb3.Name = "cb3";
+            this.cb3.Size = new System.Drawing.Size(121, 21);
+            this.cb3.TabIndex = 2;
+            // 
+            // cb4
+            // 
+            this.cb4.FormattingEnabled = true;
+            this.cb4.Items.AddRange(new object[] {
+            "Sim",
+            "Não"});
+            this.cb4.Location = new System.Drawing.Point(434, 126);
+            this.cb4.Name = "cb4";
+            this.cb4.Size = new System.Drawing.Size(121, 21);
+            this.cb4.TabIndex = 3;
+            // 
+            // cb5
+            // 
+            this.cb5.FormattingEnabled = true;
+            this.cb5.Items.AddRange(new object[] {
+            "Sim",
+            "Não"});
+            this.cb5.Location = new System.Drawing.Point(434, 153);
+            this.cb5.Name = "cb5";
+            this.cb5.Size = new System.Drawing.Size(121, 21);
+            this.cb5.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(434, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Resultado";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(120, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Telefonou para a Vitima?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(120, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Esteve no local do crime?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(120, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Mora perto da vitima?";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(120, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(235, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Devia algum valor para a vitima?";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(120, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Já trabalhou com a vitima?";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 289);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cb5);
+            this.Controls.Add(this.cb4);
+            this.Controls.Add(this.cb3);
+            this.Controls.Add(this.cb2);
+            this.Controls.Add(this.cb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.ComboBox cb2;
+        private System.Windows.Forms.ComboBox cb3;
+        private System.Windows.Forms.ComboBox cb4;
+        private System.Windows.Forms.ComboBox cb5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Ex1_Pag15_10/Ex1_Pag15_10/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex1_Pag15_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(cb1.Text == "" || cb2.Text == "" || cb3.Text == "" || cb4.Text == "" || cb5.Text == "")
+            {
+                MessageBox.Show("Por favor responda as questões.");
+            }
+            else
+            {
+                int part = 0;
+                if(cb1.Text == "Sim")
+                {
+                    part++;
+                }
+                if (cb2.Text == "Sim")
+                {
+                    part++;
+                }
+                if (cb3.Text == "Sim")
+                {
+                    part++;
+                }
+                if (cb4.Text == "Sim")
+                {
+                    part++;
+                }
+                if (cb5.Text == "Sim")
+                {
+                    part++;
+                }
+
+                if(part == 2)
+                {
+                    MessageBox.Show("Suspeito.");
+                }
+                else if(part >= 3 && part <= 4)
+                {
+                    MessageBox.Show("Cúmplice");
+                }
+                else if(part == 5)
+                {
+                    MessageBox.Show("Assassino.");
+                }
+                else
+                {
+                    MessageBox.Show("Inocente");
+                }
+            }
+        }
+    }
+}
